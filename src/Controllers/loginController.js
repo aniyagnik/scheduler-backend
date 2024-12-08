64dfe596d37fd298d01f4d4fd296cd13d7af969c
aniyagnik <ani789yagnik@gmail.com>
2024-12-08 20:22:07 +0530

    fixed login profile

--- src/Controllers/loginController.js
+++ src/Controllers/loginController.js
@@ -9,6 +9,11 @@ const authUser = (request, accessToken, refreshToken, profile, done) => {
   console.log("done : ", done)
   console.log("accessToken : ", accessToken)
   console.log("refreshToken : ", refreshToken)
+  profile={
+    username:refreshToken.displayName,
+    emailId:refreshToken.emails[0].value,
+
+  }
   return done(null, profile);
 }
 
@@ -21,14 +26,16 @@ passport.use(new GoogleStrategy({
 }, authUser));
 
 passport.serializeUser( (user, done) => { 
-  console.log(`Serialize User: ${user}`)
+  console.log(`Serialize User: `)
+  console.log(user)
   // The USER object is the "authenticated user" from the done() in authUser function.
   // serializeUser() will attach this user to "req.session.passport.user.{user}", so that it is tied to the session object for each session.  
   done(null, user)
 } )
 
 passport.deserializeUser((user, done) => {
-  console.log(`Deserialized User : ${user}`)
+  console.log(`Deserialized User : `)
+  console.log(user)
   // This is the {user} that was saved in req.session.passport.user.{user} in the serializationUser()
   // deserializeUser will attach this {user} to the "req.user.{user}", so that it can be used anywhere in the App.
   done (null, user)
--- src/Databases/index.js
+++ src/Databases/index.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 import * as taskDatabase from './taskDatabase.js';
 
-mongoose.connect(process.env.MONGO_URL || 'mongodb://localhost:27017/myDatabase')
+mongoose.connect(process.env.MONGO_URL)
     .then(result => console.log('connected to Mongodb'))
     .catch(err => console.error(err));
 
