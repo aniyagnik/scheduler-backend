b21906eabeb223649d3d2b9051b2d015f5874771
aniyagnik <ani789yagnik@gmail.com>
2024-12-09 11:00:22 +0530

    models folder

--- src/models/taskModel.js
+++ src/models/taskModel.js
@@ -1,25 +1,39 @@
 import mongoose from 'mongoose';
 
-const taskSchema = new mongoose.Schema({
-  title: String, // task name
-  priority:Number, // priority
-  startDate: { type: Date, default: Date.now() }, 
+const taskModel = new mongoose.Schema({
+  title: {
+    type: String,
+    require:true
+  }, // task name
+  priority:{
+    type: Number,
+    require:true
+  }, // priority
+  startDate: { type: Date, default: Date.now(),require:true }, 
   endDate: { type: Date, default: Date.now() },
   type: {
     type: String,
     enum: ["measurable", "yes or no"],
+    require:true
   }, // measurable or yes/no
-  unit: String, // hours,rep, kms etc. can be null 
+  unit: {
+    type: String,
+    require:true
+  }, // hours,rep, kms etc. can be null 
   targetType: {
     type: String,
     enum: ["atleast", "atmost"],
+    require:true
   }, // atleast or atmost
-  target: String, // target value 
+  target: {
+    type: Number,
+    require:true
+  }, // target value 
   score: [Number], // score achieved
   pointsAssigned: Number, // points earned by partial/complete fulfillment of task
   // points gained can be calculated by score[i]/target * pointsAssigned
 });
 
-const Task = mongoose.model('Task', taskSchema);
+const Task = mongoose.model('Task', taskModel);
 
 export default Task;
